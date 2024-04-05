9740e9c3fcd969844cdefb9448b24fb3f402606e
Phillip Gachnang <pgachnang@gmail.com>
2024-04-05 10:32:46 +0200

    changed models

--- main_gui_hotel_search.py
+++ main_gui_hotel_search.py
@@ -5,43 +5,22 @@ from pathlib import Path
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication
 
+from sqlalchemy import create_engine
+
 from sqlalchemy import create_engine, func
 from sqlalchemy.schema import CreateTable
-
+from data_access.data_base import *
 from data_access.data_generator import *
 from gui.hotel_search import *
 
-def init_db() -> Engine:
-    data_path = Path(os.getcwd()).joinpath("data")
-    data_path.mkdir(exist_ok=True)
 
-    connection_str = f"sqlite:///{data_path}/example.db"
-    e = create_engine(connection_str)
-    with open(data_path.joinpath("example.ddl"), "w") as ddl_file:
-        for table in Base.metadata.tables.values():
-            create_table = str(CreateTable(table).compile(e)).strip()
-            ddl_file.write(f"{create_table};{os.linesep}")
+DB_PATH = './data/hotel_reservation.db'
 
-    Base.metadata.drop_all(e)
-    Base.metadata.create_all(e)
-    return e
-
-def generate_example_data(e: Engine):
-    generate_system_data(e)
-    generate_hotels(e)
-    generate_guests(e)
-    generate_registered_guests(e)
 
 
 def main():
-    engine = init_db()
-    generate_example_data(engine)
-
-    # Handle high resolution displays:
-    if hasattr(Qt, 'AA_EnableHighDpiScaling'):
-        QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
-    if hasattr(Qt, 'AA_UseHighDpiPixmaps'):
-        QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
+    init_db(DB_PATH, True, True, True)
+    engine = create_engine(f"sqlite:///{DB_PATH}")
 
     with Session(engine) as session:
         app = QApplication(sys.argv)
